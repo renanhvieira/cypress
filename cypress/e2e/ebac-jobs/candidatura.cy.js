69fcccf3e18e465daaf1a72afeb2600bbc2eee34
renanhvieira <renanhvieira@hotmail.com>
2025-08-21 16:24:28 -0300

    tarefa2

--- cypress/e2e/ebac-jobs/candidatura.cy.js
+++ cypress/e2e/ebac-jobs/candidatura.cy.js
@@ -9,6 +9,7 @@ describe('Testes para a página de candidatura', () => {
     it('Deve levar o usuário até o formulário de inscrição', () => {
          cy.get('.Vaga_vagaLink_DeFkk').first().click()
          cy.get('input').should('have.length',7)
+         cy.screenshot('tela-inscricao')
 })
 
         it('Deve preenchero formulário de inscrição', () => {
@@ -24,5 +25,7 @@ describe('Testes para a página de candidatura', () => {
         cy.on('window:alert', (conteudo) => {
             expect(conteudo).contain('Obrigado pela candidatura!')
         })
+
+        cy.screenshot('tela-inscricao-preenchido')
     })
 })
